4ea742143663d46809b5f839d8e2bf0ed64206e7
mohammedafridh <afridh7788@gmail.com>
2024-11-24 07:09:03 +0530

    adding a testing change

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -9,6 +9,7 @@ const useLogin = () => {
     const login = async(username,password)=>{
         setError(null)
         setIsLoading(true)
+        console.log("hi")
 
         const response = await fetch('/users/login', {
         method:'POST',
